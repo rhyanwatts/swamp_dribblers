0d1266ffa3fcaea7670fd43d8267c3398e39d036
Matt Flack <s3493444@student.rmit.edu.au>
2017-07-29 18:54:57 +1000

    comment out tests that broke due to previous bikesack overhaul

--- src/BikeSack/TestingClasses/IndicatorsTest.java
+++ src/BikeSack/TestingClasses/IndicatorsTest.java
@@ -3,129 +3,131 @@ import BikeSack.BikeSack;
 
 public class IndicatorsTest {
 
-    public static void main(String[] args) {
-        BikeSack bikeSack = new BikeSack();
-        
-        // Test Left
-        System.out.println("Indicating Left");
-        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
-        
-        // Test Right
-        System.out.println("\nIndicating Right");
-        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
-
-        // Test toggle right off
-        System.out.println("\nIndicating Right (toggle off)");
-        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
-        
-        // Left on to test toggle off
-        System.out.println("\nIndicating Left");
-        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
-        
-        // Test toggle left off
-        System.out.println("\nIndicating Left (toggle off)");
-        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
-        
-        // Right on to test turning off
-        System.out.println("\nIndicating Right");
-        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
-        
-        // Test turning off
-        System.out.println("\nTurning Off");
-        bikeSack.toggleIndicator(IndicatorDirection.NONE);
-
-        // Left on to test turning off
-        System.out.println("\nIndicating Left");
-        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
-
-        // Test turning off
-        System.out.println("\nTurning Off");
-        bikeSack.toggleIndicator(IndicatorDirection.NONE);
-        
-        // Test Fading Left
-        System.out.println("\nIndicating Left to Test Fading");
-        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        
-     // Test Fading Right
-        System.out.println("\nIndicating Right to Test Fading");
-        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-        bikeSack.updateIndicatorInstruments();
-        bikeSack.updateDisplay();
-
-    }
+    // Broken since bikesack class rework
+    
+//    public static void main(String[] args) {
+//        BikeSack bikeSack = new BikeSack();
+//        
+//        // Test Left
+//        System.out.println("Indicating Left");
+//        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
+//        
+//        // Test Right
+//        System.out.println("\nIndicating Right");
+//        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
+//
+//        // Test toggle right off
+//        System.out.println("\nIndicating Right (toggle off)");
+//        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
+//        
+//        // Left on to test toggle off
+//        System.out.println("\nIndicating Left");
+//        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
+//        
+//        // Test toggle left off
+//        System.out.println("\nIndicating Left (toggle off)");
+//        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
+//        
+//        // Right on to test turning off
+//        System.out.println("\nIndicating Right");
+//        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
+//        
+//        // Test turning off
+//        System.out.println("\nTurning Off");
+//        bikeSack.toggleIndicator(IndicatorDirection.NONE);
+//
+//        // Left on to test turning off
+//        System.out.println("\nIndicating Left");
+//        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
+//
+//        // Test turning off
+//        System.out.println("\nTurning Off");
+//        bikeSack.toggleIndicator(IndicatorDirection.NONE);
+//        
+//        // Test Fading Left
+//        System.out.println("\nIndicating Left to Test Fading");
+//        bikeSack.toggleIndicator(IndicatorDirection.LEFT);
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        
+//     // Test Fading Right
+//        System.out.println("\nIndicating Right to Test Fading");
+//        bikeSack.toggleIndicator(IndicatorDirection.RIGHT);
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//        bikeSack.updateIndicatorInstruments();
+//        bikeSack.updateDisplay();
+//
+//    }
 
 }
