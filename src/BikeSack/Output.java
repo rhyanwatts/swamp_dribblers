8c32490f35e6cc1a34fb8bac88f38f6885779788
Aidan Holmes <s3355003@student.rmit.edu.au>
2017-08-14 23:47:01 +0800

    Output.java commenting and Javadoc updated

--- src/BikeSack/Output.java
+++ src/BikeSack/Output.java
@@ -1,178 +1,257 @@
 package BikeSack;
 
 /**
- * <h1>Bike Sack Output Class</h1> The Output class defines an object that can
- * behave as any of the outputs within the BikeSack motorcycle system.
+ * <h1>Bike Sack Output</h1> Defines an object that will control any of the
+ * outputs within the BikeSack motorcycle system.
  * 
- * <h2>Use as a dimmable (PWM) type output</h2> Outputs can have a value of 0 -
- * 255 set for use with PWM output for dimming. The output can use
+ * <h2>Use as a dimmable (PWM) type output</h2> Value for this object can range
+ * between 0 - 255 for use with PWM output for dimming. The output can use
  * Output.setLevel(int level) to set a custom level. Output.inc() and
  * Output.dec() will increment and decrement the output level by a default value
- * of 25, however a custom increment/decrement step can be defined by using
- * Output.setIncrementStep(int increment);
+ * of 25, however a custom increment/decrement step can be also be defined by
+ * using Output.setIncrementStep(int increment);
  * 
- * <h2>Use as a binary on/off type output</h2> Use Output.toggle(); to simply
- * switch on or off.
+ * <h2>Use as a binary on/off type output</h2>
+ * <p>
+ * - Use Output.toggle(); to simply switch on or off.
+ * </p>
  * 
- * Alternatively By using Output.setLevel(Output.on); or
- * Output.setLevel(Output.off); the output can behave in a binary on/off
- * fashion.
+ * <p>
+ * - Alternatively By using Output.setLevel(Output.on); or
+ * Output.setLevel(Output.off)
+ * </p>
  * 
- * Alternatively, if Output.setIncrementStep(255); is used, then Output.inc();
- * and Output.dec(); will also turn the output on/off.
+ * <p>
+ * - Finally, if Output.setIncrementStep(255); is used, then Output.inc(); and
+ * Output.dec(); will also turn the output on/off.
+ * </p>
  * 
  * @author Aidan Holmes s3355003
  * @since 11/7/2017
- * @version 0.2
+ * @version 1.0
  */
 
 public class Output {
 
-	// set publicly accessible constants for simple output levels
-	public static final int OFF = 0;
-	public static final int MED = 128;
-	public static final int ON = 255;
+   /**
+    * @param OFF
+    *           defines Output off value of 0
+    * @param MED
+    *           defines Output medium value of 128
+    * @param ON
+    *           defines Output maximum value of 255
+    * 
+    */
+   public static final int OFF = 0;
+   public static final int MED = 128;
+   public static final int ON = 255;
 
-	// declare private variables
-	private String outputType;
-	private int outputLevel;
-	private int incrementStep;
+   // declare private variables
+   private String outputType;
+   private int outputLevel;
+   private int incrementStep;
 
-	// Default Constructor
-	public Output() {
-		outputType = null;
-		outputLevel = OFF;
-		incrementStep = 25; // Default = approx 10 steps for full range
-	}
+   /**
+    * This contstructs an Output with default values. Output level = Off, Increment
+    * size = 25
+    */
+   public Output() {
+      outputType = null;
+      outputLevel = OFF;
+      incrementStep = 25; // Default = approx 10 steps for full range
+   }
 
-	// Overloaded Constructor including type string
-	public Output(String type) {
-		this(); // Call default constructor
-		outputType = type;
-	}
+   /**
+    * THis constructs an Output with a name.
+    * 
+    * @param type
+    *           Name of output type e.g. "Brake Light"
+    */
+   public Output(String type) {
+      this(); // Call default constructor
+      outputType = type;
+   }
 
-	// Overloaded Constructor including type and level
-	public Output(String type, int level) {
-		this(); // Call default constructor
-		outputType = type;
-		outputLevel = level;
-	}
+   /**
+    * This constructs an Output with a name & initial value.
+    * 
+    * @param type
+    *           Name of output type e.g. "Brake Light"
+    * @param level
+    *           Initial Output level (0 - 255)
+    */
+   public Output(String type, int level) {
+      this(); // Call default constructor
+      outputType = type;
+      outputLevel = level;
+   }
 
-	// Overloaded Constructor including type, level and Increment Size
-	public Output(String type, int level, int increment) {
-		this(); // Call default constructor
-		outputType = type;
-		outputLevel = level;
-		incrementStep = increment;
-	}
+   /**
+    * This constructs an Output with a name, initial value and increment step size.
+    * 
+    * @param type
+    *           Name of output type e.g. "Brake Light"
+    * @param level
+    *           Initial Output level (0 - 255)
+    * @param increment
+    *           Increment step size (default = 25)
+    */
+   public Output(String type, int level, int increment) {
+      this(); // Call default constructor
+      outputType = type;
+      outputLevel = level;
+      incrementStep = increment;
+   }
 
-	// Getter - returns output level
-	public int getOutputLevel() {
-		return outputLevel;
-	}
+   /**
+    * @return The current Output level of this object.
+    */
+   public int getOutputLevel() {
+      return outputLevel;
+   }
 
-	// Getter - returns output type
-	public String getOutputType() {
-		return outputType;
-	}
+   /**
+    * @return The name of this Output
+    */
+   public String getOutputType() {
+      return outputType;
+   }
 
-	// Getter - returns increment size
-	public int getIncrementStep() {
-		return incrementStep;
-	}
+   /**
+    * @return The increment step size for this Output
+    */
+   public int getIncrementStep() {
+      return incrementStep;
+   }
 
-	// Setter - set output level, returns true if successful
-	public boolean setoutputLevel(int level) {
-		if (level >= OFF && level <= ON) {
-			outputLevel = level;
-			writeState();
-		}
-		boolean result = (outputLevel == level) ? true : false;
-		return result;
-	}
+   /**
+    * @param level
+    *           The new output level for this Output
+    * @return True if setting output value was successful. Otherwise False.
+    */
+   public boolean setoutputLevel(int level) {
+      // validate input in valid range
+      if (level >= OFF && level <= ON) {
+         outputLevel = level;
+         writeState();
+      }
+      boolean result = (outputLevel == level) ? true : false;
+      return result;
+   }
 
-	// Setter - sets increment step
-	public boolean setIncrementStep(int increment) {
-		if (increment >= OFF && increment <= ON) {
-			incrementStep = increment;
-			writeState();
-		}
-		boolean result = (incrementStep == increment) ? true : false;
-		return result;
-	}
+   /**
+    * @param increment
+    *           The new increment step size for this Output
+    * @return True if setting increment value was successful. Otherwise False.
+    */
+   public boolean setIncrementStep(int increment) {
+      if (increment >= OFF && increment <= ON) {
+         incrementStep = increment;
+         writeState();
+      }
+      boolean result = (incrementStep == increment) ? true : false;
+      return result;
+   }
 
-	// Increments output Level. Returns True if successful.
-	public boolean inc() {
-		int temp = outputLevel;
-		if (outputLevel + incrementStep <= ON) {
-			outputLevel += incrementStep;
-		} else {
-			outputLevel = ON;
-		}
-		if (outputLevel > temp) {
-			writeState();
-			return true;
-		} else {
-			return false;
-		}
-	}
+   /**
+    * Increment Output value by current increment step size (default = 25)
+    * 
+    * @return True if incrementing output value was successful. Otherwise False.
+    */
+   public boolean inc() {
+      int temp = outputLevel;
+      // if closer to ON than the increment, just turn ON
+      if (outputLevel + incrementStep <= ON) {
+         outputLevel += incrementStep;
+      } else {
+         outputLevel = ON;
+      }
+      if (outputLevel > temp) {
+         writeState();
+         return true; // if value was increased
+      } else {
+         return false; // if value was not increased
+      }
+   }
 
-	// Decrements output Level. Returns True if successful.
-	public boolean dec() {
-		int temp = outputLevel;
-		if (outputLevel - incrementStep >= OFF) {
-			outputLevel -= incrementStep;
-		} else {
-			outputLevel = OFF;
-		}
-		if (outputLevel < temp) {
-			writeState();
-			return true;
-		} else {
-			return false;
-		}
-	}
+   /**
+    * Decrement Output value by current increment step size (default = 25)
+    * 
+    * @return True if decrementing output value was successful. Otherwise False.
+    */
+   public boolean dec() {
+      int temp = outputLevel;
+      // if closer to OFF than the increment, just turn OFF
+      if (outputLevel - incrementStep >= OFF) {
+         outputLevel -= incrementStep;
+      } else {
+         outputLevel = OFF;
+      }
+      if (outputLevel < temp) {
+         writeState();
+         return true; // if value was decreased
+      } else {
+         return false; // if value was not decreased
+      }
+   }
 
-	// Checks if the output is currently on
-	public boolean isOn() {
-		if (outputLevel > 0) {
-			return true;
-		}
-		return false;
-	}
+   /**
+    * 
+    * @return True if the Output level is higher than OFF (0)
+    */
+   public boolean isOn() {
+      if (outputLevel > 0) {
+         return true;
+      }
+      return false;
+   }
 
-	// Toggles outputLevel between on and off for boolean type operation
-	public void toggle() {
-		if (outputLevel > OFF) {
-			outputLevel = OFF;
-			writeState();
-		} else if (outputLevel == OFF) {
-			outputLevel = ON;
-			writeState();
-		}
-	}
+   /**
+    * Toggles or flip / flops the Output value between OFF and ON.
+    * <p>
+    * If current Output > 0, first action is to turn OFF.
+    * </p>
+    * <p>
+    * Easy implementation of binaly ON/OFF behaviour.
+    * </p>
+    */
+   public void toggle() {
+      if (outputLevel > OFF) {
+         outputLevel = OFF;
+         writeState();
+      } else if (outputLevel == OFF) {
+         outputLevel = ON;
+         writeState();
+      }
+   }
 
-	// Write state to console for state change requirement / validation
-	private void writeState() {
-		/*
-		 * TODO - comment out all calls to this method once validation has been
-		 * completed and the Text GUI has been written
-		 */
-		System.out.println(this.toString());
-	}
+   /**
+    * Writes current state of object to Console for A3 validation testing.
+    */
+   private void writeState() {
+      /*
+       * TODO - comment out all calls to this method once validation has been
+       * completed and the Text GUI has been written
+       */
+      System.out.println(this.toString());
+   }
 
-	@Override
-	public String toString() {
-		// prepare state in a human readable form
-		String state;
-		if (outputLevel == OFF) {
-			state = "OFF";
-		} else if (outputLevel == ON) {
-			state = "ON";
-		} else {
-			state = "Dimmed to " + Integer.toString(outputLevel) + "/255";
-		}
-		// Modify eclipses auto-generated toString() return statement for our purpose
-		return "Output [" + (outputType != null ? "Type= " + outputType + ", " : "") + "State= " + state + "]";
-	}
+   /**
+    * @return Output name and current output value in human readable String.
+    *         <p>e.g. "Type=Brake Light, State= ON"</p>
+    */
+   @Override
+   public String toString() {
+      // prepare state in a human readable form
+      String state;
+      if (outputLevel == OFF) {
+         state = "OFF";
+      } else if (outputLevel == ON) {
+         state = "ON";
+      } else {
+         state = "Dimmed to " + Integer.toString(outputLevel) + "/255";
+      }
+      // Modify eclipses auto-generated toString() return statement for our purpose
+      return "Output [" + (outputType != null ? "Type= " + outputType + ", " : "") + "State= " + state + "]";
+   }
 }
