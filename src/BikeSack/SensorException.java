35d70764ad549152eb8d241a5e397f8f4aa240e3
Steven Grueber <s3502715@student.rmit.edu.au>
2017-08-18 23:24:49 +1000

    Add JavaDoc to SensorException

--- src/BikeSack/SensorException.java
+++ src/BikeSack/SensorException.java
@@ -1,8 +1,17 @@
 package BikeSack;
 
+/**
+ * Exception that is thrown when the Sensor is set to an invalid value
+ * @author SwampDribblers
+ */
 @SuppressWarnings("serial")
 public class SensorException extends Exception {
 
+	/**
+	 * Create a new <code>SensorException</code> with the defined message
+	 * @param message The message to contain in the Exception
+	 * @see Exception
+	 */
 	public SensorException(String message) {
 		super(message);
 	}
